a5d75ebe4247ae836a973e69ba2bec26d3d4adbf
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-09-29 10:03:31 +0300

    feat: update global style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,19 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  #root {
+    width: 100%;
+    height: 100%;
+  }
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
   /* Document
     ========================================================================== */
 
